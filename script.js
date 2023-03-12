2920b0119d5049138b8dc806eb7e056b6d524d40
IordanisKokk <72551397+IordanisKokk@users.noreply.github.com>
2023-03-12 15:50:22 +0200

    create ui for the rps game

--- script.js
+++ script.js
@@ -1,16 +1,41 @@
+const gameContainer = document.querySelector('.gamecontainer');
+
+const btnImages = ['resources/rock.svg', 'resources/paper.svg', 'resources/scissors.svg']
+
+const rockButton = new Image(100, 100);
+const paperButton = new Image(100, 100);
+const scissorsButton = new Image(100, 100);
+
+const buttons = [rockButton, paperButton, scissorsButton]
+
+const startButton = document.querySelector('.startButton');
+const header1 = document.querySelector('#header1');
+const header2 = document.querySelector('#header2');
+const winnerHeader = document.createElement('h3');
+
+const headerContainer = document.querySelector('.header-container');
+const battleContainer = document.querySelector('.battleContainer');
+const roundContainer = document.querySelector('.roundContainer');
+
+let roundNumber = 0;
+
 let userScore = 0;
 let compScore = 0;
 
-function getComputersChoice(){
+let finished = false;
+
+let playersChoice;
+
+function getComputersChoice() {
 
     let rand = Math.floor(Math.random() * 10) % 3;
     let choice;
 
-    if(rand === 0){
+    if (rand === 0) {
         choice = 'rock';
-    }else if(rand === 1){
+    } else if (rand === 1) {
         choice = 'paper';
-    }else{
+    } else {
         choice = 'scissors';
     }
 
@@ -18,69 +43,181 @@ function getComputersChoice(){
 
 }
 
-function round(playerSelection, computerSelection){
-
+function round(playerSelection, computerSelection) {
+    roundNumber++;
     let roundWinner = null;
-    if(playerSelection === computerSelection){
-        return "Tie!";
-    }else{
-        if(playerSelection === "rock"){
-            if(computerSelection === "scissors"){
-                roundWinner = "player";
-            }else{
-                roundWinner = "comp";
-            }
-        }else if(playerSelection === "paper"){
-            if(computerSelection === "rock"){
-                roundWinner = "player";
-            }else{
-                roundWinner = "comp";
-            }
-        }else {
-            if(computerSelection === "paper"){
-                roundWinner = "player";
-            }else{
-                roundWinner = "comp";
-            }
-        }
-    }
-    if(roundWinner != null){
-        if(roundWinner === "player"){
-            userScore++;
-            return ("You win! " + playerSelection + " beats " + computerSelection +"!");
-        }else{
-            compScore++;
-            return ("You lose! " + computerSelection + " beats " + playerSelection +"!");
-        }
-    }
-}
-
-function game(){
-
-    while(userScore < 5 && compScore < 5){
-        let userInput;
-        while(userInput != 'rock' && userInput != 'paper' && userInput != 'scissors' ){
-            userInput = prompt("Select Rock, Paper or Scissors");
-            userInput = userInput.toLowerCase();
-        }
-        console.log("You have selected " + userInput + "!");
-        console.log(round(userInput, getComputersChoice()))
-        console.log("SCORE")
-        console.log("USER: " + userScore + " COMPUTER: " + compScore);
-    }
+    let result;
     
-    if(compScore === 5){
-        console.log("Computer Wins! Better luck next time...");
-    }else if(userScore === 5){
-        console.log("You Win! Congratulations! :)");
+    if (playerSelection === computerSelection) {
+        result = "Tie!";
+    } else {
+        if (playerSelection === "rock") {
+            if (computerSelection === "scissors") {
+                roundWinner = "player";
+            } else {
+                roundWinner = "comp";
+            }
+        } else if (playerSelection === "paper") {
+            if (computerSelection === "rock") {
+                roundWinner = "player";
+            } else {
+                roundWinner = "comp";
+            }
+        } else {
+            if (computerSelection === "paper") {
+                roundWinner = "player";
+            } else {
+                roundWinner = "comp";
+            }
+        }
+    }
+    if (roundWinner != null) {
+        if (roundWinner === "player") {
+            userScore++;
+            result = ("You win! " + playerSelection + " beats " + computerSelection + "!");
+        } else {
+            compScore++;
+            result = ("You lose! " + computerSelection + " beats " + playerSelection + "!");
+        }
+    }
+    return result;
+}
+
+function checkIfGameIsFinished() {
+    if (userScore >= 5 || compScore >= 5) {
+        gameContainer.remove(rockButton);
+        gameContainer.remove(paperButton);
+        gameContainer.remove(scissorsButton);
+        roundContainer.remove();
+        winnerHeader.classList.add('winnerHeader');
+        if(userScore === 5){
+            winnerHeader.textContent = 'YOU WIN!!!';
+        }else{
+            winnerHeader.textContent = 'YOU LOSE!!! Try again';
+        }
+        headerContainer.replaceChildren(winnerHeader)
+        headerContainer.append(header2);
+        
+        headerContainer.append(startButton)
+        userScore = 0;
+        compScore = 0;
+        roundNumber = 0;
     }
 
 }
 
+function updateScoreBoard(){
+    header2.textContent = `SCORE: ${userScore} - ${compScore} `
+}
 
-let playGameButton = document.querySelector('.btn');
-playGameButton.addEventListener("click", function(){
-    game();
+function getPlayersChoiceAndPlayRound(e) {
+    playersChoice = (this.id);
+    let computersChoice = getComputersChoice();
+    console.log('Player: ' + playersChoice);
+    console.log('Computer: ' + computersChoice);
+
+    result = round(playersChoice, computersChoice);
+    console.log(result);
+    renderRound(playersChoice, computersChoice, result);
+    updateScoreBoard();
+    checkIfGameIsFinished();
+}
+
+function renderRound(playerChoice, computerChoice, result){
+    let content = document.querySelector('.content');
+    content.appendChild(roundContainer);
+    const playerImageWrapper = document.createElement('div');
+    playerImageWrapper.classList.add('imagewrapper');
+    playerImageWrapper.id = 'player';
+
+    const compImageWrapper = document.createElement('div');
+    compImageWrapper.classList.add('imagewrapper');
+    compImageWrapper.id = 'comp';
+
+    const playerChoiceImage = new Image(100,100);
+    playerChoiceImage.src = `resources/${playerChoice}.svg`;
+    const playerHeader = document.createElement('h3');
+    playerHeader.textContent = 'YOU';
+    const playerChoiceLabel = document.createElement('p');
+    playerChoiceLabel.textContent = playerChoice.toUpperCase();
+    playerImageWrapper.appendChild(playerHeader);
+    playerImageWrapper.appendChild(playerChoiceImage);
+    playerImageWrapper.appendChild(playerChoiceLabel);
+
+    const compChoiceImage = new Image(100,100);
+    compChoiceImage.src = `resources/${computerChoice}.svg`;
+    const compHeader = document.createElement('h3');
+    compHeader.textContent = 'COMP';
+    const compChoiceLabel = document.createElement('p');
+    compChoiceLabel.textContent = computerChoice.toUpperCase();
+    compImageWrapper.appendChild(compHeader);
+    compImageWrapper.appendChild(compChoiceImage);
+    compImageWrapper.appendChild(compChoiceLabel);
+    
+    console.log(playerImageWrapper);
+    console.log(battleContainer);
+    console.log("HAS CHILD NODES: " + battleContainer.hasChildNodes())
+    
+    if(battleContainer.firstElementChild === null){
+        battleContainer.appendChild(playerImageWrapper);
+        battleContainer.appendChild(compImageWrapper);
+        console.log(battleContainer.firstElementChild);
+    }else{
+        battleContainer.replaceChildren(playerImageWrapper, compImageWrapper);
+    }
+
+    const roundResults = document.querySelector('.results');
+    roundResults.textContent = result;
+
+    const roundHeader = document.querySelector('.round');
+    roundHeader.textContent = `ROUND ${roundNumber}`
+
+
+
+}
+
+function renderButtons() {
+
+    console.log("rendering Buttons...");
+
+    rockButton.id = 'rock';
+    paperButton.id = 'paper';
+    scissorsButton.id = 'scissors';
+
+    if(roundNumber === 0){
+        let i = 0;
+        buttons.forEach(button => {
+            button.src = btnImages[i];
+            button.classList.add('playButton')
+            gameContainer.appendChild(button);
+            i++
+        });
+    
+        buttons.forEach(button => {
+            button.addEventListener('click', getPlayersChoiceAndPlayRound)
+        });
+    }
+    else{
+        buttons.forEach(button =>{
+            gameContainer.appendChild(button);
+        });
+    }
+
+
+}
+
+startButton.addEventListener("click",  () =>{
+    console.log('button clicked')
+    header1.textContent = 'PLAYING ROCK, PAPER, SCISSORS!!!';
+
+    if(headerContainer.firstElementChild === winnerHeader){
+        headerContainer.replaceChild(header1, winnerHeader);
+        let content = document.querySelector('.content');
+        content.appendChild(gameContainer);
+    }
+
+    header2.textContent = 'SCORE: 0 - 0';
+    startButton.remove();
+    renderButtons();
 });
 
-game();
